cadd1dc0b1f65a354e768f4e3b43b113707de4f0
Andrey <andriei.shtyrov@mail.ru>
2021-02-27 00:06:51 +0300

    Make blanks for buildings and Weather

--- Controller/Building.cs
+++ Controller/Building.cs
@@ -1,45 +1,6 @@
 ﻿namespace Controller
 {
-    public class Building
-    {
-        public BuildingTypes Type;
-        public bool isOwned;
-        private Player _owner;
-        public Player owner
-        {
-            get
-            {
-                if ( Type != BuildingTypes.Contryside)
-                { return _owner; }
-                return null;
-            }
-        }
-
-        public (int X, int Y) fieldPosition;
-
-        public Building((int X, int Y) fpos)
-        {
-            fieldPosition = fpos;
-            if (Type == BuildingTypes.Camp || Type == BuildingTypes.Contryside)
-                isOwned = false;
-            isOwned = true;
-        }
-
-        public event ChangeOwnerHandler ChangeOwner;
-
-        public void setPlayer(Player player)
-        {
-            if (player != owner)
-            {
-                var prevOwner = _owner;
-                _owner = player;
-                ChangeOwner?.Invoke(this, prevOwner);
-            }
-        }
-    }
-
-    
-
+  
     public enum BuildingTypes
     {
         Camp = 0,
@@ -49,5 +10,4 @@
         MoveFlag = 4,
     }
 
-    public delegate void ChangeOwnerHandler(Building sender, Player prevOwner);
 }
